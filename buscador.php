10d1bbfb7d4c51c61b0d6c2ac392a6cee664c0e1
unknown <alumno@alumno-sw3.(none)>
2012-03-13 21:50:49 -0300

    Mi primera vez...

---
+++ buscador.php
@@ -0,0 +1,21 @@
+<?php
+class Buscador extends CI_Controller{
+  function __construct(){
+    parent::__construct();
+    $this->load->model("buscador/buscador_model");
+    $this->load->helpers("form");
+  }
+
+  public function index(){
+    $this->load->view("buscador/buscador_view");
+  }
+  
+public function like(){
+   $patrona = $this->input->post("patron");
+   $data["resultado"] = $this->buscador_model->getData($patrona);
+   $this->load->view("buscador/buscador_view",$data);
+  }
+
+
+}
+?>	
